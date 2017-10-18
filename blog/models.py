0a9ca1db730eb6acf7128270415e43a966d3587f
oneweekonething <oneweekonething@gmail.com>
2017-10-18 16:25:33 +0800

    refs #3, add model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,149 @@
 from django.db import models
+from uuslug import slugify
+from django.contrib.sites.models import Site
+from django.conf import settings
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+# 参考各类参数涵义:http://blog.csdn.net/devil_2009/article/details/41735611
+# 参考内部类Meta:http://www.cnblogs.com/ccorz/p/Django-models-zhong-demeta-xuan-xiang.html
+
+
+class BaseModel(models.Model):
+    slug = models.SlugField(default='no-slug', max_length=60, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug or self.slug == 'no-slug' or not self.id:
+            slug = self.title if 'title' in self.__dict__ else self.name
+            self.slug = slugify(slug)
+        super().save(*args, **kwargs)
+
+    def get_full_url(self):
+        site = Site.objects.get_current().domain
+        url = "https://{site}{path}".format(site=site, path=self.get_absolute_url)
+        return url
+
+    class Meta:
+        abstract = True
+
+
+class Article(BaseModel):
+    """文章"""
+    STATUS_CHOICES = (
+        ('d', '草稿'),
+        ('p', '发表'),
+    )
+    COMMENT_STATUS = (
+        ('o', '打开'),
+        ('c', '关闭'),
+    )
+    TYPE = (
+        ('a', '文章'),
+        ('p', '页面'),
+    )
+    title = models.CharField('标题', max_length=200, unique=True)
+    body = models.TextField('正文')
+    created_time = models.DateTimeField('创建时间', auto_now_add=True)
+    last_mod_time = models.DateTimeField('修改时间', auto_now=True)
+    pub_time = models.DateTimeField('发布时间', blank=True, null=True)
+    status = models.CharField('文章状态', max_length=1, choices=STATUS_CHOICES, default='p')
+    comment_status = models.CharField('评论状态', max_length=1, choices=COMMENT_STATUS, default='o')
+    type = models.CharField('类型', max_length=1, choices=TYPE, default='a')
+    views = models.PositiveIntegerField('浏览量', default=0)
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='作者', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', verbose_name='分类', on_delete=models.CASCADE, blank=True, null=True)
+    tags = models.ManyToManyField('Tag', verbose_name='标签集合', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-pub_time']
+        verbose_name = "文章"
+        verbose_name_plural = verbose_name
+        get_latest_by = 'created_time'
+
+    def get_absolute_url(self):
+        return reverse('blog:detailbyid', kwargs={
+            'article_id': self.id,
+            'year': self.created_time.year,
+            'month': self.created_time.month,
+            'day': self.created_time.day
+        })
+
+
+class Category(BaseModel):
+    """文章分类"""
+    name = models.CharField('分类名', max_length=30, unique=True)
+    created_time = models.DateTimeField('创建时间', auto_now_add=True)
+    last_mod_time = models.DateTimeField('修改时间', auto_now=True)
+    parent_category = models.ForeignKey('self', verbose_name='父类分类', blank=True, null=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def get_absolute_url(self):
+        return reverse('blog:category_detail', kwargs={'category_name': self.slug})
+
+    def __str__(self):
+        return self.name
+
+    def get_category_tree(self):
+        """
+        递归获得分类目录的父级
+        :return:
+        """
+        categorys = []
+
+        def parse(category):
+            categorys.append(category)
+            if category.parent_category:
+                parse(category.parent_category)
+
+        parse(self)
+        return categorys
+
+    def get_sub_categorys(self):
+        """
+        获得当前分类目录所有子集
+        :return:
+        """
+
+        categorys = []
+        all_categorys = Category.objects.all()
+
+        def parse(category):
+            if category not in categorys:
+                categorys.append(category)
+            childs = all_categorys.filter(parent_category=category)
+            for chid in childs:
+                if category not in categorys:
+                    categorys.append(chid)
+                parse(chid)
+
+        parse(self)
+        return categorys
+
+
+class Tag(BaseModel):
+    """文章标签"""
+    name = models.CharField('标签名', max_length=30, unique=True)
+    created_time = models.DateTimeField('创建时间', auto_now_add=True)
+    last_mod_time = models.DateTimeField("修改时间", auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('blog:tag_detail', kwargs={'tag_name': self.slug})
+
+    def get_article_count(self):
+        return Article.objects.filter(tags__name=self.name).distinct().count()
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
--- oauth/models.py
+++ oauth/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+
+
+class OAuthUser(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='用户', blank=True, null=True)
+    openid = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50, verbose_name='昵称')
+    picture = models.CharField(max_length=350, blank=True, null=True)
+    type = models.CharField(blank=False, null=False, max_length=50)
+    email = models.CharField(max_length=50, null=True, blank=True)
+    token = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.nickname
+
+    class Meta:
+        verbose_name = 'oauth用户'
+        verbose_name_plural = verbose_name
