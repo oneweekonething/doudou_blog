f61caa385fba74b68ca585a3db430d972eaf97f8
oneweekonething <oneweekonething@gmail.com>
2017-11-02 17:19:40 +0800

    refs #3, add rock mover

--- blog/static/blog/js/blog.js
+++ blog/static/blog/js/blog.js
@@ -17,3 +17,43 @@ function cancel_reply() {
     $("#id_parent_comment_id").val('')
     $("#commentform").appendTo($("#respond"));
 }
+
+
+/** 侧边栏回到顶部 */
+var rocket = $('#rocket');
+
+$(window).on('scroll', debounce(slideTopSet, 300));
+
+function debounce(func, wait) {
+	var timeout;
+	return function() {
+		clearTimeout(timeout);
+		timeout = setTimeout(func, wait);
+	};
+};
+function slideTopSet() {
+	var top = $(document).scrollTop();
+
+	if (top > 200) {
+		rocket.addClass('show');
+	} else {
+		rocket.removeClass('show');
+	}
+}
+$(document).on('click', '#rocket', function(event) {
+	rocket.addClass('move');
+	$('body, html').animate({
+		scrollTop: 0
+	}, 800);
+});
+$(document).on('animationEnd', function() {
+	setTimeout(function() {
+		rocket.removeClass('move');
+	}, 400);
+
+});
+$(document).on('webkitAnimationEnd', function() {
+	setTimeout(function() {
+		rocket.removeClass('move');
+	}, 400);
+});
